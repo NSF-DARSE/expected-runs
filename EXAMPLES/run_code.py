5e0ec3208e47152c534f9b9abcff1fc1d78960d4
Abhin Suresh <abhinsuresh01@gmail.com>
2026-06-02 14:03:54 -0400

    error fix: 1

--- EXAMPLES/run_code.py
+++ EXAMPLES/run_code.py
@@ -6,22 +6,22 @@ import glob
 
 from expected_runs.generate_gamestate_summary import *
  
-
+# /path/to/trackman/data
 base_path = "../../v3"
 years = ["2024", "2025"]
-out_dir = "../python_files/results/CSV_files"
+
+out_dir = "../expected_runs/results/CSV_files"
 
 summary_df = build_gamestate_summary_all_years(base_path, out_dir)
 
+# returns a list of gamestate.csv files choose one in next fun.
 summary_path = glob.glob(out_dir + "GameState_*.csv")
 
 print(summar_path)
 
-"""
 final_df = build_final_dataset(
                 base_path,
                 years=["2024", "2025"],
-                summary_path,
+                summary_path[0],
                 out_dir,
                 save=True)
-"""
---
+++ expected_runs/conference_teams.py
@@ -0,0 +1,97 @@
+cusa_teams = [
+    "DAL_PAT",   # Dallas Baptist
+    "WES_HIL",   # WKU / Western Kentucky
+    "KEN_OWL",   # Kennesaw State
+    "JAC_GAM",   # Jax State
+    "LOU_BUL",   # LA Tech
+    "FLO_PAN",   # FIU
+    "NMS_AGG",   # NM State
+    "LIB_FLA",   # Liberty
+    "MTSU_BLU",  # MTSU
+    "SAM_BEA",   # Sam Houston
+    "UTS_ROA",   # UTSA
+    "CHA_FOR",   # Charlotte
+    "FAU_OWL",   # FAU
+    "RIC_OWL",   # Rice
+    "UAB_BLA"    # UAB
+]
+
+sunbelt_teams = [
+    "APP_MOU",   # App State
+    "ASU_RED",   # Arkansas State
+    "COA_CHA",   # Coastal Carolina
+    "GEO_EAG",   # Georgia Southern
+    "GEO_PAN",   # Georgia State
+    "JMU_DUK",   # James Madison
+    "LOU_CAJ",   # Louisiana
+    "MAR_THU",   # Marshall
+    "OLD_MON",   # Old Dominion
+    "SAL_JAG",   # South Alabama
+    "SOU_GOL",   # Southern Miss
+    "TEX_BOB",   # Texas State
+    "TRO_TRJ",   # Troy
+    "ULM_WAR"    # ULM
+]
+
+american_teams = [
+    "ECU_PIR",   # East Carolina
+    "HOU_COU",   # Houston
+    "WIC_SHO",   # Wichita State
+    "UCF_KNI",   # UCF
+    "CIN_BEA",   # Cincinnati
+    "MT",        # Memphis
+    "TUL_GRE",   # Tulane
+    "USF_BUL",   # South Florida
+    "UTS_ROA",   # UTSA
+    "UAB_BLA",   # UAB
+    "FAU_OWL",   # Florida Atlantic
+    "CHA_FOR",   # Charlotte
+    "RIC_OWL"    # Rice
+]
+
+all_three_conference_teams = [
+    # C-USA
+    "DAL_PAT",   # Dallas Baptist
+    "WES_HIL",   # WKU / Western Kentucky
+    "KEN_OWL",   # Kennesaw State
+    "JAC_GAM",   # Jax State
+    "LOU_BUL",   # LA Tech
+    "FLO_PAN",   # FIU
+    "NMS_AGG",   # NM State
+    "LIB_FLA",   # Liberty
+    "MTSU_BLU",  # MTSU
+    "SAM_BEA",   # Sam Houston
+
+    # Shared C-USA / American
+    "UTS_ROA",   # UTSA
+    "CHA_FOR",   # Charlotte
+    "FAU_OWL",   # FAU
+    "RIC_OWL",   # Rice
+    "UAB_BLA",   # UAB
+
+    # Sun Belt
+    "APP_MOU",   # App State
+    "ASU_RED",   # Arkansas State
+    "COA_CHA",   # Coastal Carolina
+    "GEO_EAG",   # Georgia Southern
+    "GEO_PAN",   # Georgia State
+    "JMU_DUK",   # James Madison
+    "LOU_CAJ",   # Louisiana
+    "MAR_THU",   # Marshall
+    "OLD_MON",   # Old Dominion
+    "SAL_JAG",   # South Alabama
+    "SOU_GOL",   # Southern Miss
+    "TEX_BOB",   # Texas State
+    "TRO_TRJ",   # Troy
+    "ULM_WAR",   # ULM
+
+    # American only
+    "ECU_PIR",   # East Carolina
+    "HOU_COU",   # Houston
+    "WIC_SHO",   # Wichita State
+    "UCF_KNI",   # UCF
+    "CIN_BEA",   # Cincinnati
+    "MT",        # Memphis
+    "TUL_GRE",   # Tulane
+    "USF_BUL"    # South Florida
+]
